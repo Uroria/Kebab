77670ce6db4c53337416bc392550559d0a531ea3
verklickt <verklickttwitch@gmx.de>
2023-07-06 20:55:22 +0200

    depends: Misc dependency moves

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,13 +24,24 @@ subprojects {
     val jetbrainsAnnotationsVersion: String by project.extra
     val lombokVersion: String by project.extra
     val slf4jVersion: String by project.extra
+    val googleGuavaVersion: String by project.extra
+    val adventureVersion: String by project.extra
     dependencies {
+        compileOnly("com.google.guava:guava:${googleGuavaVersion}")
+
         compileOnly("org.jetbrains:annotations:${jetbrainsAnnotationsVersion}")
         compileOnly("org.projectlombok:lombok:${lombokVersion}")
         annotationProcessor("org.jetbrains:annotations:${jetbrainsAnnotationsVersion}")
         annotationProcessor("org.projectlombok:lombok:${lombokVersion}")
 
         api("org.slf4j:slf4j-api:$slf4jVersion")
+
+        compileOnlyApi("net.kyori:adventure-api:${adventureVersion}")
+        compileOnlyApi("net.kyori:adventure-text-serializer-gson:${adventureVersion}")
+        compileOnlyApi("net.kyori:adventure-text-minimessage:${adventureVersion}")
+        compileOnlyApi("net.kyori:adventure-text-serializer-plain:${adventureVersion}")
+        compileOnlyApi("net.kyori:adventure-text-serializer-legacy:${adventureVersion}")
+        compileOnlyApi("net.kyori:adventure-nbt:${adventureVersion}")
     }
 
     java.toolchain {
--- kebab-api/build.gradle.kts
+++ kebab-api/build.gradle.kts
@@ -1,5 +1,4 @@
 
 dependencies {
-    compileOnly(project(":kebab-common"))
     api(project(":kebab-protocol"))
 }
